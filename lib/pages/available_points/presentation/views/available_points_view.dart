4ba116ae6d422504639efcf78ffdd0c1341efc54
salma gamal sayed <salma.gamal@citystars.com.eg>
2022-03-27 08:40:40 +0200

    fix available points height

--- lib/pages/available_points/presentation/views/available_points_view.dart
+++ lib/pages/available_points/presentation/views/available_points_view.dart
@@ -17,73 +17,78 @@ class AvailablePointsView extends GetView<AvailablePointsController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: Get.width,
-        height: Get.height,
         color: const Color(ColorCode.primary),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.max,
           children: [
-            Container(
-              width: 420,
-              height: 210,
-              decoration: BoxDecoration(
-                color: const Color(ColorCode.darkGrayBackground),
-                borderRadius: BorderRadius.circular(20.0),
-                border: Border.all(
-                    width: 3.0, color: const Color(ColorCode.accentLightColor)),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color(ColorCode.shadowBackground),
-                    offset: Offset(0, 4),
-                    blurRadius: 4,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 420,
+                  height: 210,
+                  decoration: BoxDecoration(
+                    color: const Color(ColorCode.darkGrayBackground),
+                    borderRadius: BorderRadius.circular(20.0),
+                    border: Border.all(
+                        width: 3.0,
+                        color: const Color(ColorCode.accentLightColor)),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(ColorCode.shadowBackground),
+                        offset: Offset(0, 4),
+                        blurRadius: 4,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Countup(
-                    begin: 0,
-                    end: 200,
-                    duration: Duration(seconds: 3),
-                    separator: '',
-                    curve: Curves.easeOut,
-                    style: TextStyles.textXLarge.copyWith(
-                      color: const Color(ColorCode.whiteBackground),
-                      shadows: [
-                        const Shadow(
-                          color: Color(ColorCode.textShadowBackground),
-                          offset: Offset(0, 3),
-                          blurRadius: 10,
-                        )
-                      ],
-                    ),
-                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Countup(
+                        begin: 0,
+                        end: 200,
+                        duration: Duration(seconds: 3),
+                        separator: '',
+                        curve: Curves.easeOut,
+                        style: TextStyles.textXLarge.copyWith(
+                          color: const Color(ColorCode.whiteBackground),
+                          shadows: [
+                            const Shadow(
+                              color: Color(ColorCode.textShadowBackground),
+                              offset: Offset(0, 3),
+                              blurRadius: 10,
+                            )
+                          ],
+                        ),
+                      ),
 
-                  // CustomText(
-                  //   '0 0 0',
-                  //   textStyle: TextStyles.textXLarge.copyWith(
-                  //     color: const Color(ColorCode.whiteBackground),
-                  //     shadows: [
-                  //       const Shadow(
-                  //         color: Color(ColorCode.textShadowBackground),
-                  //         offset: Offset(0, 3),
-                  //         blurRadius: 10,
-                  //       )
-                  //     ],
-                  //   ),
-                  // ),
-                  const SizedBox(
-                    width: 12.5,
+                      // CustomText(
+                      //   '0 0 0',
+                      //   textStyle: TextStyles.textXLarge.copyWith(
+                      //     color: const Color(ColorCode.whiteBackground),
+                      //     shadows: [
+                      //       const Shadow(
+                      //         color: Color(ColorCode.textShadowBackground),
+                      //         offset: Offset(0, 3),
+                      //         blurRadius: 10,
+                      //       )
+                      //     ],
+                      //   ),
+                      // ),
+                      const SizedBox(
+                        width: 12.5,
+                      ),
+                      SvgPicture.asset(
+                        'assets/images/icon_coins.svg',
+                        fit: BoxFit.fill,
+                      ),
+                    ],
                   ),
-                  SvgPicture.asset(
-                    'assets/images/icon_coins.svg',
-                    fit: BoxFit.fill,
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
             const SizedBox(
               height: 30,
