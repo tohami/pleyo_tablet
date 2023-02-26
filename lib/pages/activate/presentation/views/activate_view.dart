f6b8bf2222305c4faa59d6e237a76c5b1bd36971
tohami hussin <tohami.hussin@uni-do.com>
2023-02-26 04:04:56 +0200

    add dropdown value selection

--- lib/pages/activate/presentation/views/activate_view.dart
+++ lib/pages/activate/presentation/views/activate_view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:number_slide_animation/number_slide_animation.dart';
 import 'package:pleyo_tablet_app/consts/colors.dart';
 import 'package:pleyo_tablet_app/consts/text_styles.dart';
+import 'package:pleyo_tablet_app/model/strapi/activation_screen.dart';
 import 'package:pleyo_tablet_app/widgets/custom_text.dart';
 import 'package:pleyo_tablet_app/widgets/custom_text_form_field.dart';
 
@@ -144,36 +145,113 @@ class ActivateView extends GetView<ActivateController> {
                                 Row(
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: field.options!
-                                      .map((e) {
-                                        bool isSelected = state[field.key] == e.value ;
-                                        return GestureDetector(
-                                    onTap: ()=> controller.activationFormValue[field.key!] = e.value!,
-                                        child: Container(
-                                              width: 64,
-                                              height: 64,
-                                              margin: EdgeInsets.all(12),
-                                              decoration: BoxDecoration(
-                                                color: e.value!.toColor()?.withOpacity(isSelected ? 1.0 : 0.5),
-                                                borderRadius:
-                                                    BorderRadius.circular(20.0),
-                                                border: isSelected ? Border.all(
-                                                    width: 3.0,
-                                                    color: const Color(ColorCode
-                                                        .accentLightColor)) : null,
-                                                // boxShadow: [
-                                                //   BoxShadow(
-                                                //     color: e.value!.toColor(),
-                                                //     offset: Offset(0, 4),
-                                                //     blurRadius: 4,
-                                                //   ),
-                                                // ],
-                                              ),
-                                            ),
-                                      );
-                                      })
-                                      .toList(),
+                                  children: field.options!.map((e) {
+                                    bool isSelected =
+                                        state[field.key] == e.value;
+                                    return GestureDetector(
+                                      onTap: () => controller
+                                              .activationFormValue[field.key!] =
+                                          e.value!,
+                                      child: Container(
+                                        width: 64,
+                                        height: 64,
+                                        margin: EdgeInsets.all(12),
+                                        decoration: BoxDecoration(
+                                          color: e.value!
+                                              .toColor()
+                                              ?.withOpacity(
+                                                  isSelected ? 1.0 : 0.5),
+                                          borderRadius:
+                                              BorderRadius.circular(20.0),
+                                          border: isSelected
+                                              ? Border.all(
+                                                  width: 3.0,
+                                                  color: const Color(ColorCode
+                                                      .accentLightColor))
+                                              : null,
+                                          // boxShadow: [
+                                          //   BoxShadow(
+                                          //     color: e.value!.toColor(),
+                                          //     offset: Offset(0, 4),
+                                          //     blurRadius: 4,
+                                          //   ),
+                                          // ],
+                                        ),
+                                      ),
+                                    );
+                                  }).toList(),
                                 ),
+                              ],
+                            ),
+                          );
+                        case "dropdown":
+                          return Container(
+                            width: 420,
+                            height: 120,
+                            margin: EdgeInsets.only(top: 30),
+                            decoration: BoxDecoration(
+                              color: const Color(ColorCode.darkGrayBackground),
+                              borderRadius: BorderRadius.circular(20.0),
+                              border: Border.all(
+                                  width: 3.0,
+                                  color:
+                                      const Color(ColorCode.accentLightColor)),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color(ColorCode.shadowBackground),
+                                  offset: Offset(0, 4),
+                                  blurRadius: 4,
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 350,
+                                  height: 100,
+                                  child: Center(
+                                    child: DropdownButton<String>(
+                                      value: controller.activationFormValue[field.key!],
+                                      items: field.options!
+                                          .map((e) => DropdownMenuItem<String>(
+                                              value: e.value!,
+                                              child: Container(
+                                                padding: EdgeInsets.only(left: 16 ),
+                                                child: CustomText(
+                                                  e.text!,
+                                                  textStyle: TextStyles.textLarge
+                                                      .copyWith(
+                                                    fontSize: 32,
+                                                    // height: 2.066666666666667,
+                                                  ),
+                                                ),
+                                              )))
+                                          .toList(),
+                                      onChanged: (i) => {
+                                        controller.activationFormValue[field.key!] = i!
+                                      },
+                                      dropdownColor: Colors.black,
+                                      isExpanded: true,
+                                      itemHeight: 100,
+                                    ),
+                                    // child: CustomTextFormField(
+                                    //   hint: field.title!,
+                                    //   keyboardType: field.type == "text"
+                                    //       ? TextInputType.text
+                                    //       : TextInputType.number,
+                                    //   initialValue: controller
+                                    //       .activationFormValue[field.key!],
+                                    //   onChanged: (value) {
+                                    //     controller.activationFormValue[
+                                    //     field.key!] = value ?? "";
+                                    //   },
+                                    //   fontSize: 30,
+                                    //   fontColor: Colors.white,
+                                    // ),
+                                  ),
+                                )
                               ],
                             ),
                           );
